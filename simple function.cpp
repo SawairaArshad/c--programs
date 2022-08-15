e2020f879c7c73cbd2e3a5d352e5e9d1e387e55d
Sawaira Arshad <110373378+SawairaArshad@users.noreply.github.com>
2022-08-15 22:49:36 +0500

    Create simple function.cpp

---
+++ simple function.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include<iostream>
+using namespace std;
+void MyMessage()
+
+  {
+    cout<<"++++++++++++++++++++++++++\n";
+    
+}
+    
+int main()
+{
+    int i;
+    int j;
+    cout<<"please enter a first value"<<endl;
+    
+    cin>>i;
+    MyMessage();
+    cout<<"please enter  a second value"<<endl;
+    cin>>j;
+    MyMessage();
+}
+
+  
