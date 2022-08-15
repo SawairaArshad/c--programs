65feeaf9bca5df1d253892ede36210a8ece6664b
Sawaira Arshad <110373378+SawairaArshad@users.noreply.github.com>
2022-08-15 22:02:26 +0500

    Create Function.cpp

---
+++ Function.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void line();
+int main() 
+{
+    line();
+    cout<<"Hello"<<endl;
+    line();
+    cout<<"We are studying function"<<endl;
+    line();
+}
+void line()
+{
+    for( int a=1; a<=20; a++)
+    cout<<"*";
+    cout<<endl;
+}
+ 
